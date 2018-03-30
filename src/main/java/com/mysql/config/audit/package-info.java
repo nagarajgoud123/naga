8ba28719721643c379a5515f028d7dc1187b9c87
nagaraj <m.nagaraj@simbalabs.com>
2018-03-30 12:13:50 +0530

    Initial application generated by JHipster-4.12.0

---
+++ src/main/java/com/mysql/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.mysql.config.audit;
---
+++ src/main/java/com/mysql/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.mysql.repository;
+
+import com.mysql.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
